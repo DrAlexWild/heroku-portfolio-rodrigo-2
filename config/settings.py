d10859454be73e6ff2b8bafe522c52e28010a8a9
RodrigoSimoes-22001628 <a22001628@alunos.ulht.pt>
2022-05-27 19:14:50 +0100

    Tiaguinho

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
-from environs import Env 
+from environs import Env
+
 env = Env()
 env.read_env()
 
@@ -25,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
